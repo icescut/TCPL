306b44893150cd048e1e88c37c1a37a6220dd4ba
梁于冰 <337988934@qq.com>
2016-08-31 15:45:33 +0800

    fix loop error

--- exercise/2-4.c
+++ exercise/2-4.c
@@ -4,7 +4,7 @@
 void squeeze(char s1[], char s2[]){
     int i, j, k;
     for(i = k = 0; s1[i] != '\0'; i++){
-        for(j = 0; s2[j] != '\0'; j++)
+        for(j = 0; s2[j] != '\0' && s2[j] != s1[i]; j++)
             ;
         if(s2[j] == '\0')
             s1[k++] = s1[i];
