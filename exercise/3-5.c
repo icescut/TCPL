0f15bbe1ccb92a5301e1b4b4464684781e3faf21
梁于冰 <337988934@qq.com>
2016-09-02 16:18:40 +0800

    Create 3-5.c

---
+++ exercise/3-5.c
@@ -0,0 +1,15 @@
+	/**
+ * 题目: itob(n, s, b),转换整数为b进制,b只能为2-36位
+ */
+void itos(int n, char s[], int b){
+    int i, sign, temp;
+ 
+    i = 0;
+    do {
+        temp = n % b;
+        temp = (temp > 0) ? temp : -temp;
+        s[i++] = (temp > 9) ? temp - 10 + 'a' : -temp + '0';
+    } while((n /= b) != 0);
+    s[i] = '\0';
+    reverse(s);
+}
