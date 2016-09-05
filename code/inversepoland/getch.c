bf01ef801e347fa8064f28262298b3bdda648235
梁于冰 <337988934@qq.com>
2016-09-05 11:31:59 +0800

    Create getch.c

---
+++ code/inversepoland/getch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "calc.h"
+
+#define BUFSIZE 100
+int bufp = 0;
+char buf[BUFSIZE]
+
+int getch(void){
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+    if(bufp < BUFSIZE)
+        buf[bufp++] = c;
+    else
+        printf("ungetch: buf is full\n");
+}
