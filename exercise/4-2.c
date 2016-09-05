c007721dca3e986b97ec0ddf91509c30a85e078d
梁于冰 <337988934@qq.com>
2016-09-05 16:07:03 +0800

    Create 4-2.c

---
+++ exercise/4-2.c
@@ -0,0 +1,39 @@
+/**
+ * atof函数：把字符串s转换为相应的双精度浮点数支持指数
+ */
+#include <ctype.h>
+
+double atof(char s[]) {
+    //power为小数位对应的10的幂
+    double val, power,power2, e, factor;
+    int i, sign;
+
+    //跳过空白符
+    for(i = 0; isspace(s[i]); i++)
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if(s[i] == '+' || s[i] == '-')
+        i++;
+
+    for(val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+    if(s[i] == '.')
+        i++;
+    for(power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+
+    if(s[i] == 'e' || s[i] == 'E')
+        i++;
+    factor = s[i] == '-' ? 0.1, 10;
+    if(s[i] == '+' || s[i] == '-')
+        i++;
+    for(e = 0; isdigit(s[i]); i++)
+        e = 10 * e + (s[i] - '0');
+    power2 = 1;
+    for(i = 1; i <= e; i++)
+        power2 *= factor;
+
+    return sign * val / power * power2;
+}
