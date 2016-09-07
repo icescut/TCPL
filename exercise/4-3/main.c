8c1ff4fe9fed1792f2ae06351e08d754a2ef5558
梁于冰 <337988934@qq.com>
2016-09-07 09:54:43 +0800

    Create main.c

---
+++ exercise/4-3/main.c
@@ -0,0 +1,50 @@
+/**
+ * 题目: 修改逆波兰计算器，使能处理取模及负数。
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "calc.h"
+
+#define MAXOP 100
+
+int main(){
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while((type = getop(s)) != EOF)
+        switch(type){
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '/':
+            op2 = pop();
+            if(op2 != 0.0)
+                push(pop() - op2);
+            else
+                printf("erroe:zero divisor\n");
+            break;
+        case '%':
+            op2 = pop();
+            if(op2 != 0.0)
+                push(fmod(pop() - op2));
+            else
+                printf("erroe:zero divisor\n");
+            break;
+        default:
+            printf("error:unknown command\n");
+            break;
+        }
+    return 0;
+}
