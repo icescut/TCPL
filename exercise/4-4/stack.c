47f7c290d01f85b3986b14c2b8b4f85f085b5b40
梁于冰 <337988934@qq.com>
2016-09-07 10:17:23 +0800

    Create stack.c

---
+++ exercise/4-4/stack.c
@@ -0,0 +1,27 @@
+	#include "calc.h"
+
+#define MAXVAL 100
+
+static int sp = 0;
+static double val[MAXVAL];
+
+
+void push(double f){
+    if(sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("stack is full, can' t push\n");
+}
+
+double pop(void){
+    if(sp > 0)
+        return val[--sp];
+    else {
+        printf("stack is empty, can't pop\n");
+        return 0.0;
+    }
+}
+
+void clear(){
+    sp = 0;
+}
