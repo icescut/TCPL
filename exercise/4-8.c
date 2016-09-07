042ab6a73042b1cdf733b0af9e319f5d759cb6b3
梁于冰 <337988934@qq.com>
2016-09-07 16:27:16 +0800

    Create 4-8.c

---
+++ exercise/4-8.c
@@ -0,0 +1,20 @@
+	/**
+ * 题目: 改写getch,ungetch，缓冲区只有一个字符
+ */
+#include <stdio.h>
+#include "calc.h"
+
+char buf = 0;
+
+int getch(void){
+    int c = (buf != 0) ? buf : getchar();
+    buf = 0;
+    return c;
+}
+
+void ungetch(int c){
+    if(buf == 0)
+        buf = c;
+    else
+        printf("ungetch: buf is full\n");
+}
