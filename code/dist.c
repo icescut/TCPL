ef3d5c9efcb06004671cbd4541ee0698283041c6
梁于冰 <337988934@qq.com>
2016-09-09 16:58:20 +0800

    Create dist.c

---
+++ code/dist.c
@@ -0,0 +1,18 @@
+/**
+ * 求点到原点间距离
+ */
+#include <math.h>
+#include <stdio.h>
+
+struct Point {
+    double x;
+    double y;
+};
+
+int main(){
+    struct Point pt = {3.0, 4.0};
+    double dist;
+    dist = sqrt(pt.x * pt.x + pt.y * pt.y);
+    printf("%g\n", dist);
+    return 0;
+}
