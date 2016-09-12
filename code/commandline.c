c0de6c0508ffbc0c5279e186ad043d600a380a9e
梁于冰 <337988934@qq.com>
2016-09-12 10:44:27 +0800

    Create commandline.c

---
+++ code/commandline.c
@@ -0,0 +1,12 @@
+/**
+ * 打印命令行输入
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int i;
+    for(i = 1; i < argc; ++i)
+        printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
+    printf("\n");
+    return 0;
+}
