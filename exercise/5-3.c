5ec6204d42998e2df42acd2b95cc58ad75a594ae
梁于冰 <337988934@qq.com>
2016-09-13 16:58:16 +0800

    更新指针错误

--- exercise/5-3.c
+++ exercise/5-3.c
@@ -2,8 +2,10 @@
  * 题目: 用指针方式实现strcat
  */
 void strcat(char *s, char *t) {
-    while(*s++)
-        ;
+    //while(*s++) 注意:s将指向\0后面一位
+      //  ;
+    while(*s)
+        s++;
     while(*s++ = *t++)
         ;
 }
