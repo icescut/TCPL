188cf3d93c59fe60e8364974ef58d6f1c9b08669
梁于冰 <337988934@qq.com>
2016-09-19 15:05:17 +0800

    Create 7-3.c

---
+++ exercise/7-3.c
@@ -0,0 +1,44 @@
+	/**
+  * 题目: 扩展minprintf，使与printf的功能更接近
+ */
+#include <stdarg.h>
+
+void minprintf(char *fmt, ...) {
+    va_list ap;
+    int *ival, i;
+    double *dval;
+    char *sval, *p = fmt;
+    char subfmt[20];
+    
+    for(va_start(ap,fmt);*p;p++) {
+        if(*p != '%') {
+            putchar(*p);
+            continue;
+        }
+        i = 0;
+        subfmt[0] = '%';
+        while(!isalpha(subfmt[++i] = *++p)
+            ;
+        sunfmt[++i] = '\0';
+        switch(*p) {
+        case 'd':
+        case 'i':
+            ival = va_arg(ap, int);
+            printf(subfmt, ival);
+            break;
+        case 'f':
+        case 'g':
+            dval = va_arg(ap, double);
+            printf(subfmt, dval);
+            break;
+        case 's':
+            sval = va_arg(ap, char *);
+            printf(subfmt, sval);
+            break;
+        default:
+            printf(subfmt);
+            break;
+        }
+    }
+    va_end(ap);
+}
