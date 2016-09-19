6555d143baa77a1c6486c93868d54200c5007e73
梁于冰 <337988934@qq.com>
2016-09-19 10:33:32 +0800

    Create cat2.c

---
+++ code/cat2.c
@@ -0,0 +1,31 @@
+/**
+ * 模仿unix的cat命令
+#include <stdio.h>
+void filecopy(FILE *, FILE *);
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    char *prog = argv[0];
+    if(argc == 1)
+        filecopy(stdin, stdout);
+    else {
+        while(--argc > 0) {
+            if((fp = fopen(*++argv)) == NULL) {
+                fprintf(stderr,"%s: can't open %s\n, prog, *argv);
+                exit(1);
+            } else  {
+                filecopy(fp, stdout);
+                fclose(fp);
+            }
+        }
+    }
+    if(ferror(stdout)) {
+        fprintf(stderr,"%s: error in writing stdout\n, prog);
+        exit(2);
+    }
+    return 0;
+}
+void filecopy(FILE *ifp, FILE *ofp) {
+    int c;
+    while((c = getc(ifp)) != EOF)
+        putc(c);
+}
